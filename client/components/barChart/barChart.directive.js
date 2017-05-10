afd5fe3f47162361192b71cdc7a940972ca79a93
Omar Vladimir Galicia Palmeros <thewakosmanclub@gmail.com>
2017-05-10 09:56:04 -0500

    Linter

--- client/components/barChart/barChart.directive.js
+++ client/components/barChart/barChart.directive.js
@@ -15,100 +15,105 @@ export default angular.module('idhAngularApp.barChart', [])
 
       link: function(scope, element, attrs) {
         d3Service.d3()
-        .then(function(d3) {
+          .then(function(d3) {
+            var margin = parseInt(attrs.margin, 10) || 250;
+            var barHeight = parseInt(attrs.barHeight, 10) || 20;
+            var barPadding = parseInt(attrs.barPadding, 10) || 5;
+            console.log(scope.year);
+            // var year = '1990'
+            var selectYear = dataByYear(scope.data, scope.year);
 
-          var margin = parseInt(attrs.margin) || 250,
-          barHeight = parseInt(attrs.barHeight) || 20,
-          barPadding = parseInt(attrs.barPadding) || 5;
-          console.log(scope.year);
-          // var year = '1990'
-          var selectYear = dataByYear(scope.data, scope.year);
+            var svg = d3.select(element[0])
+              .append('svg')
+              .style('width', '100%');
 
-          var svg = d3.select(element[0])
-            .append('svg')
-            .style('width', '100%');
-
-          // Browser onresize event
-          window.onresize = function() {
-            scope.$apply();
-          };
+            // Browser onresize event
+            window.onresize = function() {
+              scope.$apply();
+            };
 
 
-          // Watch for resize event
-          scope.$watch(function(e) {
-            console.log(e);
-            selectYear = dataByYear(scope.data, e.year);
-            scope.render(selectYear);
-            // return angular.element($window)[0].innerWidth;
-          });
+            // Watch for resize event
+            scope.$watch(function(e) {
+              console.log(e);
+              selectYear = dataByYear(scope.data, e.year);
+              console.log(selectYear);
 
-          scope.render = function(data) {
-            // remove all previous items before render
-            svg.selectAll('*').remove();
+              scope.render(selectYear);
+              // return angular.element($window)[0].innerWidth;
+            });
 
-            // If we don't pass any data, return out of the element
-            if (!data) return;
+            scope.render = function(data) {
+              // remove all previous items before render
+              svg.selectAll('*').remove();
 
-            // setup variables
-            var width = d3.select(element[0]).node().offsetWidth - margin,
-            // calculate the height
-            height = scope.data.length * (barHeight + barPadding),
-            // our xScale
-            xScale = d3.scale.linear()
-            .domain([0, d3.max(data, function(d) {
-              return d.idh;
-            })])
-            .range([0, width]);
+              // If we don't pass any data, return out of the element
+              if(!data) return;
 
-            // set the height based on the calculations above
-            svg.attr('height', height);
+              // setup variables
+              var width = d3.select(element[0]).node().offsetWidth - margin;
+                // calculate the height
+              var height = scope.data.length * (barHeight + barPadding);
+                // our xScale
+              var xScale = d3.scale.linear()
+                .domain([0, d3.max(data, function(d) {
+                  return d.idh;
+                })])
+                .range([0, width]);
 
-            //create the rectangles for the bar chart
-            svg.selectAll('rect')
-              .data(data).enter()
+              // set the height based on the calculations above
+              svg.attr('height', height);
+
+              //create the rectangles for the bar chart
+              svg.selectAll('rect')
+                .data(data)
+                .enter()
                 .append('rect')
                 .attr('height', barHeight)
                 .attr('width', 140)
-                .attr('x', Math.round(margin/2))
-                .attr('y', function(d,i) {
+                .attr('x', Math.round(margin / 2))
+                .attr('y', function(d, i) {
                   return i * (barHeight + barPadding);
                 })
-                .attr('fill', d3.rgb(48,144,168))
+                .attr('fill', d3.rgb(48, 144, 168))
                 .transition()
-                  .duration(500)
-                  .attr('width', function(d) {
-                    return xScale(d.idh);
-                  });
+                .duration(500)
+                .attr('width', function(d) {
+                  return xScale(d.idh);
+                });
 
-            // add Name to each Bar
-            svg.selectAll('text')
-            .data(data).enter()
-            .append('text')
-            .attr('fill', d3.rgb(0,0,0))
-            .attr('y', function(d,i) {
-              return i * (barHeight + barPadding) + 15;
-            })
-            .attr('x', 0)
-            .text(function(d) {
-              return d.name;
-            });
-          }
-        });
+              // add Name to each Bar
+              svg.selectAll('text')
+                .data(data)
+                .enter()
+                .append('text')
+                .attr('fill', d3.rgb(0, 0, 0))
+                .attr('y', function(d, i) {
+                  return i * (barHeight + barPadding) + 15;
+                })
+                .attr('x', 0)
+                .text(function(d) {
+                  return d.name;
+                });
+            };
+          });
 
-        function dataByYear(data,year){
+        function dataByYear(data, year) {
           var dataYear = [];
           data.forEach(state => {
-            state.years.forEach(element =>{
-              if (element.year == year) {
-                var obj = {name: state.name, idh: element.idh};
+            state.years.forEach(ele => {
+              if(ele.year == year) {
+                var obj = {
+                  name: state.name,
+                  idh: ele.idh
+                };
                 dataYear.push(obj);
-                return
+                return;
               }
             });
           });
           return dataYear;
         }
-
       }
     };
   })
